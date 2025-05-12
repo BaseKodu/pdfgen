b716c846008a966db84ed6a2b30fe69c16e7b09d
Phenyo <phenyoitumeleng@gmail.com>
2025-05-12 15:07:28 +0200

    set up a toast

---
+++ src/components/components/ui/AppToast.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+import { defineProps } from 'vue';
+
+const props = defineProps({
+  message: {
+    type: String,
+    default: 'Default toast message'
+  },
+  type: {
+    type: String,
+    default: 'info'
+  },
+  placement: {
+    type: String,
+    default: 'toast-start toast-middle'
+  },
+});
+</script>
+
+<template>
+  <div :class="`toast ${props.placement}`">
+    <div :class="`alert alert-${props.placement}`">
+      <p>{{ props.message }}</p>
+    </div>
+  </div>
+
+</template>
